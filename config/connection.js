46f38c98487f9d1bd8f63bdbad86035560c6131d
rrbrink <ryanbrinkerhoff@live.com>
2018-03-14 10:59:46 -0600

    heroku ready

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
   connection = mysql.createConnection({
-    host     : 'localhost',
+    host     : '127.0.0.1',
+    port     : '8889',
     user     : 'root',
     password : 'root',
     database : 'burgers_db'
